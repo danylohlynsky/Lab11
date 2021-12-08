f2ac259409c2bdee3032f33c4463e70023ce0d65
Данило Глинський <Danylo.Hlynskyi.PZ.2020@edu.lpnu.ua>
2021-12-09 00:37:57 +0200

    bug added(?)

--- Lab11/Lab11_Client.cpp
+++ Lab11/Lab11_Client.cpp
@@ -6,27 +6,33 @@
 using namespace std;
 int main()
 {
-    wcout << "Connecting to pipe..." << endl;
+    HANDLE pipe;
+    while (true) {
+        pipe = CreateFile(
+            L"\\\\.\\pipe\\my_pipe",
+            GENERIC_READ | GENERIC_WRITE,
+            FILE_SHARE_READ | FILE_SHARE_WRITE,
+            NULL,
+            OPEN_EXISTING,
+            FILE_ATTRIBUTE_NORMAL,
+            NULL
+        );
 
-    // Open the named pipe
-    // Most of these parameters aren't very relevant for pipes.
-    HANDLE pipe = CreateFile(
-        L"\\\\.\\pipe\\my_pipe",
-        GENERIC_READ | GENERIC_WRITE, // only need read access
-        FILE_SHARE_READ | FILE_SHARE_WRITE,
-        NULL,
-        OPEN_EXISTING,
-        FILE_ATTRIBUTE_NORMAL,
-        NULL
-    );
-
-    if (pipe == INVALID_HANDLE_VALUE) {
+        if (pipe == INVALID_HANDLE_VALUE)
+            break;
+        if (GetLastError() == ERROR_PIPE_BUSY)
+        {
+            if (!WaitNamedPipe(L"\\\\.\\pipe\\my_pipe", NMPWAIT_USE_DEFAULT_WAIT))
+                continue;  
+        }
+        else
+            break;
         wcout << "Failed to connect to pipe." << endl;
-        // look up error code here using GetLastError()
+        
         system("pause");
         return 1;
-    }
 
+    }
     // Sending name to server 
     wcout << "Enter your name:" << endl;
     string name;
@@ -35,74 +41,63 @@ int main()
     const wchar_t* data = widestr.c_str();
     DWORD numBytesWritten = 0;
     BOOL result = WriteFile(
-        pipe, // handle to our outbound pipe
-        data, // data to send
-        wcslen(data) * sizeof(wchar_t), // length of data to send (bytes)
-        &numBytesWritten, // will store actual amount of data sent
-        NULL // not using overlapped IO
+        pipe, 
+        data, 
+        wcslen(data) * sizeof(wchar_t),
+        &numBytesWritten, 
+        NULL
     );
 
-    wcout << "Reading data from pipe..." << endl;
-    // The read operation will block until there is data to read
     wchar_t buffer[128];
     DWORD numBytesRead = 0;
     result = ReadFile(
         pipe,
-        buffer, // the data from the pipe will be put here
-        127 * sizeof(wchar_t), // number of bytes allocated
-        &numBytesRead, // this will store number of bytes actually read
-        NULL // not using overlapped IO
+        buffer,
+        127 * sizeof(wchar_t), 
+        &numBytesRead, 
+        NULL
     );
 
     if (result) {
-        buffer[numBytesRead / sizeof(wchar_t)] = '\0'; // null terminate the string
-        wcout << "Message: " << buffer << endl;
-    }
-    else {
-        wcout << "Failed to read data from the pipe." << endl;
-    }
-    wcout << "Sending data to server..." << endl;
-
-    // This call blocks until a client process reads all the data
-    wcout << "Enter any message:" << endl;
-    string data1;
-    getline(cin, data1);
-    widestr = wstring(data1.begin(), data1.end());
-    data = widestr.c_str();
-    numBytesWritten = 0;
-    result = WriteFile(
-        pipe, // handle to our outbound pipe
-        data, // data to send
-        wcslen(data) * sizeof(wchar_t), // length of data to send (bytes)
-        &numBytesWritten, // will store actual amount of data sent
-        NULL // not using overlapped IO
-    );
-
-
-    wcout << "Reading data from pipe..." << endl;
-    // The read operation will block until there is data to read
-    buffer[128];
-    numBytesRead = 0;
-    result = ReadFile(
-        pipe,
-        buffer, // the data from the pipe will be put here
-        127 * sizeof(wchar_t), // number of bytes allocated
-        &numBytesRead, // this will store number of bytes actually read
-        NULL // not using overlapped IO
-    );
-    if (result) {
-        buffer[numBytesRead / sizeof(wchar_t)] = '\0'; // null terminate the string
+        buffer[numBytesRead / sizeof(wchar_t)] = '\0'; 
         wcout << buffer << endl;
     }
     else {
         wcout << "Failed to read data from the pipe." << endl;
     }
 
-    // Close our pipe handle
+    wcout << "Enter your message:" << endl;
+    string data1;
+    getline(cin, data1);
+    widestr = wstring(data1.begin(), data1.end());
+    data = widestr.c_str();
+    numBytesWritten = 0;
+    result = WriteFile(
+        pipe, 
+        data, 
+        wcslen(data) * sizeof(wchar_t),
+        &numBytesWritten,
+        NULL 
+    );
+
+    buffer[128];
+    numBytesRead = 0;
+    result = ReadFile(
+        pipe,
+        buffer,
+        127 * sizeof(wchar_t), 
+        &numBytesRead,
+        NULL 
+    );
+    if (result) {
+        buffer[numBytesRead / sizeof(wchar_t)] = '\0';
+        wcout << buffer << endl;
+    }
+    else {
+        wcout << "Failed to read data from the pipe." << endl;
+    }
     CloseHandle(pipe);
-
     wcout << "Done." << endl;
-
     system("pause");
     return 0;
 }
--- Lab11_Server/Lab11_Server.cpp
+++ Lab11_Server/Lab11_Server.cpp
@@ -3,11 +3,12 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#define USERS 4
 using namespace std;
 HANDLE hMutex;
 HANDLE myH = CreateMutex(0, FALSE, (LPCWSTR)"Globa\\myH");
 int counter = 0;
-string prohibitedWords[] = { "NULP", "OS", "Some", "KRUK", "other" };
+string prohibitedWords[] = { "drug", "weapon", "terrorist", "attack", "politician", "crime", "c++"};
 int prohibitedAmount = 3;
 struct pr {
     vector <string> words;
@@ -17,181 +18,181 @@ DWORD WINAPI MassageChecker(__in LPVOID params) {
     
     pr parameters = *(pr*)params;
     for (string word : parameters.words) {
-        if (word == parameters.wordToCheck)
+        if (word.compare(parameters.wordToCheck)) {
             WaitForSingleObject(myH, INFINITE);
             counter++;
             ReleaseMutex(myH);
+        }
     }
     return 0;
 }
 int main(int argc, const char** argv) {
     wcout << "Creating an instance of a named pipe..." << endl;
 
-    // Create a pipe to send data
     HANDLE pipe = CreateNamedPipe(
-        L"\\\\.\\pipe\\my_pipe", // name of the pipe
+        L"\\\\.\\pipe\\my_pipe",
         PIPE_ACCESS_DUPLEX, 
-        PIPE_TYPE_BYTE, // send data as a byte stream
-        1, // only allow 1 instance of this pipe
-        0, // no outbound buffer
-        0, // no inbound buffer
-        0, // use default wait time
-        NULL // use default security attributes
+        PIPE_TYPE_BYTE,
+        PIPE_UNLIMITED_INSTANCES, 
+        0, 
+        0, 
+        0, 
+        NULL
     );
 
     if (pipe == NULL || pipe == INVALID_HANDLE_VALUE) {
         wcout << "Failed to create outbound pipe instance.";
-        // look up error code here using GetLastError()
         system("pause");
         return 1;
     }
 
     wcout << "Waiting for a client to connect to the pipe..." << endl;
-
-    // This call blocks until a client process connects to the pipe
-    BOOL result = ConnectNamedPipe(pipe, NULL);
-    if (!result) {
-        wcout << "Failed to make connection on named pipe." << endl;
-        // look up error code here using GetLastError()
-        CloseHandle(pipe); // close the pipe
-        system("pause");
-        return 1;
-    }
-
-    wcout << "Reading name from client..." << endl;
-    // The read operation will block until there is data to read
-    wchar_t buffer[128];
-    DWORD numBytesRead = 0;
-    result = ReadFile(
-        pipe,
-        buffer, // the data from the pipe will be put here
-        127 * sizeof(wchar_t), // number of bytes allocated
-        &numBytesRead, // this will store number of bytes actually read
-        NULL // not using overlapped IO
-    );
-    const wchar_t* data = L"Your name successfully added";
-    if (result) {
-        buffer[numBytesRead / sizeof(wchar_t)] = '\0'; // null terminate the string
-        wstring ws(buffer);
-        string name(ws.begin(), ws.end());
-        ifstream myfileRead;
-        myfileRead.open("Users.txt");
-        string fileData;
-        string line;
-
-        int index = 0;
-        if (myfileRead.is_open())
-        {
-            getline(myfileRead, line);
-            while (myfileRead)
-            {
-                fileData += line;
-                getline(myfileRead, line);
-            }
+    for (int us = 0; us < USERS; us++) {
+        BOOL result = ConnectNamedPipe(pipe, NULL);
+        if (!result) {
+            wcout << "Failed to make connection on named pipe." << endl;
+            CloseHandle(pipe); 
+            system("pause");
+            return 1;
         }
-        myfileRead.close();
-        
-        if (fileData.find(name) == std::string::npos) {
-            ofstream myfile;
-            myfile.open("Users.txt", ios_base::app);
-            myfile << name + "\n";
-            myfile.close();
+
+        wcout << "Reading name from client..." << endl;
+        wchar_t buffer[128];
+        DWORD numBytesRead = 0;
+        result = ReadFile(
+            pipe,
+            buffer, 
+            127 * sizeof(wchar_t),
+            &numBytesRead,
+            NULL
+        );
+        const wchar_t* data = L"Your name successfully added";
+        if (result) {
+            buffer[numBytesRead / sizeof(wchar_t)] = '\0'; 
+            wstring ws(buffer);
+            string name(ws.begin(), ws.end());
+            ifstream myfileRead;
+            myfileRead.open("Users.txt");
+            string fileData;
+            string line;
+
+            int index = 0;
+            if (myfileRead.is_open())
+            {
+                getline(myfileRead, line);
+                while (myfileRead)
+                {
+                    fileData += line;
+                    getline(myfileRead, line);
+                }
+            }
+            myfileRead.close();
+
+            if (fileData.find(name) == std::string::npos) {
+                ofstream myfile;
+                myfile.open("Users.txt", ios_base::app);
+                myfile << name + "\n";
+                myfile.close();
+            }
+            else
+                data = L"You are already registered:)";
+
+        }
+        else {
+            wcout << "Failed to read name from the pipe." << endl;
+        }
+
+        wcout << "Sending data to pipe..." << endl;
+        DWORD numBytesWritten = 0;
+        result = WriteFile(
+            pipe, 
+            data,
+            wcslen(data) * sizeof(wchar_t), 
+            &numBytesWritten, 
+            NULL 
+        );
+
+        if (result) {
+            wcout << "Number of bytes sent: " << numBytesWritten << endl;
+        }
+        else {
+            wcout << "Failed to send data." << endl;
+        }
+
+        wcout << "Reading client message..." << endl;
+        buffer[128];
+        numBytesRead = 0;
+        result = ReadFile(
+            pipe,
+            buffer, 
+            127 * sizeof(wchar_t),
+            &numBytesRead, 
+            NULL
+        );
+
+        if (result) {
+            buffer[numBytesRead / sizeof(wchar_t)] = '\0'; 
+            wcout << "Number of bytes read: " << numBytesRead << endl;
+            wcout << "Message: " << buffer << endl;
+        }
+        else {
+            wcout << "Failed to read data from the pipe." << endl;
+        }
+        wstring ws1(buffer);
+        string message(ws1.begin(), ws1.end());
+        vector<string> words{};
+        string delimiter = " ";
+        size_t pos = 0;
+        string token;
+        while ((pos = message.find(delimiter)) != string::npos) {
+            token = message.substr(0, pos);
+            words.push_back(token);
+            message.erase(0, pos + delimiter.length());
+        }
+
+        HANDLE* h = new HANDLE[sizeof(prohibitedWords)/sizeof(*prohibitedWords)];
+        pr* par = new pr[sizeof(prohibitedWords) / sizeof(*prohibitedWords)];
+        int i = 0;
+        counter = 0;
+        for (string w : prohibitedWords) {
+            DWORD threadID;
+            par[i].words = words;
+            par[i].wordToCheck = w;
+            h[i] = CreateThread(0, 0, (LPTHREAD_START_ROUTINE)MassageChecker, (LPVOID)&par[i++], NULL, &threadID);
+        }
+        WaitForMultipleObjects(sizeof(prohibitedWords) / sizeof(*prohibitedWords), h, FALSE, INFINITE);
+
+
+        wcout << "Sending data to pipe..." << endl;
+        if (counter < prohibitedAmount)
+            data = buffer;
+        else
+            data = L"Too many prohibited words in your message!";
+
+        numBytesWritten = 0;
+        result = WriteFile(
+            pipe,
+            data,
+            wcslen(data) * sizeof(wchar_t), 
+            &numBytesWritten,
+            NULL
+        );
+
+        if (result) {
+            wcout << "Number of bytes sent: " << numBytesWritten << endl;
+        }
+        else {
+            wcout << "Failed to send data." << endl;
+        }
+        if (!DisconnectNamedPipe(pipe))
+        {
+            printf("Disconnect failed %d\n", GetLastError());
         }
         else
-            data = L"You are already registered:)";
-
+        {
+            printf("Disconnect successful\n");
+        }
     }
-    else {
-        wcout << "Failed to read name from the pipe." << endl;
-    }
-
-    wcout << "Sending data to pipe..." << endl; 
-    // This call blocks until a client process reads all the data
-    DWORD numBytesWritten = 0;
-    result = WriteFile(
-        pipe, // handle to our outbound pipe
-        data, // data to send
-        wcslen(data) * sizeof(wchar_t), // length of data to send (bytes)
-        &numBytesWritten, // will store actual amount of data sent
-        NULL // not using overlapped IO
-    );
-
-    if (result) {
-        wcout << "Number of bytes sent: " << numBytesWritten << endl;
-    }
-    else {
-        wcout << "Failed to send data." << endl;
-        // look up error code here using GetLastError()
-    }
-
-    wcout << "Reading client message..." << endl;
-    // The read operation will block until there is data to read
-    buffer[128];
-    numBytesRead = 0;
-    result = ReadFile(
-        pipe,
-        buffer, // the data from the pipe will be put here
-        127 * sizeof(wchar_t), // number of bytes allocated
-        &numBytesRead, // this will store number of bytes actually read
-        NULL // not using overlapped IO
-    );
-
-    if (result) {
-        buffer[numBytesRead / sizeof(wchar_t)] = '\0'; // null terminate the string
-        wcout << "Number of bytes read: " << numBytesRead << endl;
-        wcout << "Message: " << buffer << endl;
-    }
-    else {
-        wcout << "Failed to read data from the pipe." << endl;
-    }
-    wstring ws1(buffer);
-    string message(ws1.begin(), ws1.end());
-    vector<string> words{};
-    string delimiter = " ";
-    size_t pos = 0;
-    string token;
-    while ((pos = message.find(delimiter)) != string::npos) {
-        token = message.substr(0, pos);
-        words.push_back(token);
-        message.erase(0, pos + delimiter.length());
-    }
-    HANDLE* h = new HANDLE[prohibitedAmount];
-    pr* par = new pr[prohibitedAmount];
-    int i = 0;
-    for (string w : prohibitedWords) {
-        DWORD threadID;
-        par[i].words = words;
-        par[i].wordToCheck = w;
-        h[i] = CreateThread(0, 0, (LPTHREAD_START_ROUTINE)MassageChecker, (LPVOID)&par[i], NULL, &threadID);
-    }
-    WaitForMultipleObjects(prohibitedAmount, h, FALSE, INFINITE);
-
-
-    wcout << "Sending data to pipe..." << endl;
-    // This call blocks until a client process reads all the data
-    
-    if (counter < prohibitedAmount)
-        data = buffer;
-    else
-        data = L"Too many prohibited words in your message!";
-
-    numBytesWritten = 0;
-    result = WriteFile(
-        pipe, // handle to our outbound pipe
-        data, // data to send
-        wcslen(data) * sizeof(wchar_t), // length of data to send (bytes)
-        &numBytesWritten, // will store actual amount of data sent
-        NULL // not using overlapped IO
-    );
-
-    if (result) {
-        wcout << "Number of bytes sent: " << numBytesWritten << endl;
-    }
-    else {
-        wcout << "Failed to send data." << endl;
-        // look up error code here using GetLastError()
-    }
-    // Close the pipe (automatically disconnects client too)
     CloseHandle(pipe);
 
     wcout << "Done." << endl;
